6350b963aa0621b7d6684535d5f27a8b232da69a
Meher Yar Khan <meheryarkhan90@hotmail.com>
2019-06-06 20:50:49 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,185 @@
+//village
+class Koala extends React.Component {
+  render() {
+    return (
+      React.createElement(Box, null));
+
+  }}
+
+
+//neighborhood
+class Box extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { className: "box" },
+      React.createElement(EarLeft, null),
+      React.createElement(EarRight, null),
+      React.createElement(EyeLeft, null),
+      React.createElement(EyeRight, null),
+      React.createElement(Head, null),
+      React.createElement(Nose, null),
+      React.createElement(HairLeft, null),
+      React.createElement(HairRight, null),
+
+      React.createElement(Saying, null)));
+
+
+
+  }}
+
+
+//blocks
+class EarLeft extends React.Component {
+  render() {
+    return (
+      React.createElement("div", null,
+      React.createElement(OuterEarLeft, null),
+      React.createElement(InnerEarLeft, null)));
+
+
+  }}
+
+
+class EarRight extends React.Component {
+  render() {
+    return (
+      React.createElement("div", null,
+      React.createElement(OuterEarRight, null),
+      React.createElement(InnerEarRight, null)));
+
+
+  }}
+
+
+class EyeLeft extends React.Component {
+  render() {
+    return (
+      React.createElement("div", null,
+      React.createElement(OuterEyeLeft, null),
+      React.createElement(PupilLeft, null)));
+
+
+  }}
+
+class EyeRight extends React.Component {
+  render() {
+    return (
+      React.createElement("div", null,
+      React.createElement(OuterEyeRight, null),
+      React.createElement(PupilRight, null)));
+
+
+  }}
+
+
+//houses
+class Head extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { className: "head" }));
+
+  }}
+
+
+class Nose extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { className: "nose" }));
+
+  }}
+
+
+class HairLeft extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { className: "hair-left" }));
+
+  }}
+
+
+class HairRight extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { className: "hair-right" }));
+
+  }}
+
+
+class OuterEarRight extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { className: "outer-ear-right" }));
+
+  }}
+
+
+class InnerEarRight extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { className: "inner-ear-right" }));
+
+  }}
+
+
+class OuterEarLeft extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { className: "outer-ear-left" }));
+
+  }}
+
+
+class InnerEarLeft extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { className: "inner-ear-left" }));
+
+  }}
+
+
+class OuterEyeLeft extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { className: "outer-eye-left" }));
+
+  }}
+
+
+class PupilLeft extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { className: "pupil-left" }));
+
+  }}
+
+
+class OuterEyeRight extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { className: "outer-eye-right" }));
+
+  }}
+
+
+class PupilRight extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { className: "pupil-right" }));
+
+  }}
+
+
+class Saying extends React.Component {
+  render() {
+    return (
+      React.createElement("div", { className: "saying" },
+      React.createElement("h4", null, "\"In this world that we live in, a koala is all you need. I need one too,\" says the Koala, in despair.")));
+
+
+  }}
+
+
+//"inject" to DOM
+ReactDOM.render(
+React.createElement(Koala, null),
+document.getElementById('koala'));
